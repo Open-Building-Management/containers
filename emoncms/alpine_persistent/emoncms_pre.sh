fe014401b91ad026c3be10670efbb8958266b783
Alexjunk <alexandre.cuer@cerema.fr>
2023-08-13 21:39:51 +0200

    persistent storage next

--- emoncms/alpine_persistent/emoncms_pre.sh
+++ emoncms/alpine_persistent/emoncms_pre.sh
@@ -1,12 +1,25 @@
 #!/command/with-contenv sh
 
+NEW_INSTALL=false
+
 if ! [ -d "$EMONCMS_DATADIR" ]; then
-    echo "creating timeseries folders"
+    echo "Creating timeseries folders"
     mkdir -p "$EMONCMS_DATADIR"
     mkdir -p "$EMONCMS_DATADIR/backup"
     mkdir -p "$EMONCMS_DATADIR/backup/uploads"
     for i in $TS; do mkdir -p "$EMONCMS_DATADIR/$i"; done
     chown -R "$DAEMON" "$EMONCMS_DATADIR"
+    
 else
-    echo "timeseries folders : OK"
+    echo "Using existing timeseries"
 fi
+
+if ! [ -d "$DATA/mysql" ]; then
+    echo "Creating a new mariadb"
+    mysql_install_db --user=mysql --datadir=$DATA/mysql --skip-name-resolve --skip-test-db > /dev/null;\
+    NEW_INSTALL=true
+else
+    echo "Using existing mariadb"
+fi
+
+printf "$NEW_INSTALL" > /var/run/s6/container_environment/NEW_INSTALL
